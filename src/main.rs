8ba79dcc506324b3ac2470682e5e78d6b79546a2
L3nn0x <dragon.super@laposte.net>
2018-12-01 23:53:46 +0000

    Added command line args

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,32 @@ mod codegen_header;
 mod codegen_source;
 mod graph_passes;
 
+use std::env;
+
 fn main() -> Result<(), failure::Error> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 0 {
+        println!("Error!");
+        std::process::exit(1);
+    } else if args.len() == 1 {
+        println!("Usage: {} <file.xml (can be multiple files)>", args[0]);
+        std::process::exit(1);
+    }
     use std::fs::File;
-    let file = File::open("srv_login_req.xml")?;
-    let packet = packet_schema::Reader::load_packet(file)?;
-    let packet = flatten::flatten("./", &packet)?;
-    let packet = graph_passes::run(packet)?;
-    println!("{:?}", packet);
-    let header_output = File::create(format!("{}.h", packet.filename()))?;
-    let mut writer = writer::Writer::new(header_output);
-    let mut codegen = codegen_header::CodeHeaderGenerator::new(&mut writer);
-    codegen.generate(&packet)?;
-    let source_output = File::create(format!("{}.cpp", packet.filename()))?;
-    let mut writer = writer::Writer::new(source_output);
-    let mut codegen = codegen_source::CodeSourceGenerator::new(&mut writer);
-    codegen.generate(&packet)?;
+    for filename in args[1..].iter() {
+        let file = File::open(filename)?;
+        let packet = packet_schema::Reader::load_packet(file)?;
+        let packet = flatten::flatten("./", &packet)?;
+        let packet = graph_passes::run(packet)?;
+        println!("{:?}", packet);
+        let header_output = File::create(format!("{}.h", packet.filename()))?;
+        let mut writer = writer::Writer::new(header_output);
+        let mut codegen = codegen_header::CodeHeaderGenerator::new(&mut writer);
+        codegen.generate(&packet)?;
+        let source_output = File::create(format!("{}.cpp", packet.filename()))?;
+        let mut writer = writer::Writer::new(source_output);
+        let mut codegen = codegen_source::CodeSourceGenerator::new(&mut writer);
+        codegen.generate(&packet)?;
+    }
     Ok(())
 }
